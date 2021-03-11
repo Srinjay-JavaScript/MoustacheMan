3a03378cb475e3ff859201de72c172093f570dbe
Srinjay-JavaScript <71552411+Srinjay-JavaScript@users.noreply.github.com>
2021-03-11 16:13:07 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function preload(){
+    moustacheMan = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 function setup(){
     canvas = createCanvas(530, 430);
@@ -11,12 +12,18 @@ function setup(){
 }
 function draw(){
     image(video, 0, 0, 530, 430);
+    image(moustacheMan, noseX-45, noseY-20, 95, 70);
+
 }
 function loaded(){
     console.log("Posenet is Initialized");
 }
+var noseX,noseY;
 function poses(output){
+    
     if (output.length > 0){
+         noseX = output[0].pose.nose.x;
+         noseY = output[0].pose.nose.y;
         console.log(output);
         console.log("X is " + output[0].pose.nose.x + "\n Y is " + output[0].pose.nose.y);
     }
