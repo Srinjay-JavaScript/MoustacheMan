00b479dd6511f886018d8046b2d0d43c79423b4f
Srinjay-JavaScript <71552411+Srinjay-JavaScript@users.noreply.github.com>
2021-03-11 13:21:24 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,19 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.size(530, 430);
+    posenet = ml5.poseNet(video, loaded);
+    posenet.on("pose", poses);
     video.hide();    
 }
 function draw(){
     image(video, 0, 0, 530, 430);
+}
+function loaded(){
+    console.log("Posenet is Initialized");
+}
+function poses(output){
+    if (output.length > 0){
+        console.log(output);
+        console.log("X is " + output[0].pose.nose.x + "\n Y is " + output[0].pose.nose.y);
+    }
 }
